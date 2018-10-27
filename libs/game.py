75d24ee59e18020a2553da1e1a7295f007dc73b8
i82gaobm <i82gaobm@uco.es>
2018-10-27 13:08:27 +0200

    personaje y enemigo

--- libs/game.py
+++ libs/game.py
@@ -1,11 +1,12 @@
 class Player:
-    def __init__(self, vida,  oro ):
-        pass
-        self._vida = 
+    def __init__(self, vida=12):
+        self._vida = vida
         self._oro = 0
 
     
 class Enemigo:
-    def __init__(self, loot):
-               self._ vida= vida/2
+    def __init__(self):
+        self._vida= vida/2
+        self._loot = 1
     
+        
