f6afbd644ffa91d84baf09efdafbeaefd011e916
Antonio Muñoz Cubero <tonicubero12@gmail.com>
2018-10-27 17:20:59 +0200

    fixed some errors

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 
 from libs import usuario, dados, game, historia
 import telebot
-
+import json
 
 bot = telebot.TeleBot("TOKEN")
 
-@bot.message_handler(commands=['start', 'help'])
-def start(m):
-    cid = m.chat.id
-    m_text = "Hola, esto es una prueba"
-    bot.send_message(cid, m_text)
 
-    
+
+with open("historia_1.json", "r") as file:
+    dct = json.load(file)
+    @bot.message_handler(commands=['start', 'help'])
+    def start(m):
+        cid = m.chat.id
+        bot.send_message(cid, dct["0"]["txt"])
