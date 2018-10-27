ad2096a21d8fc9b2b79068fcd77178b9119e1ee9
TnTManu <43857650+TnTManu@users.noreply.github.com>
2018-10-27 12:56:04 +0200

    update game.py

--- libs/game.py
+++ libs/game.py
@@ -1,3 +1,11 @@
 class Player:
-    def __init__(self):
+    def __init__(self, vida,  oro ):
         pass
+        self._vida = 
+        self._oro = 0
+
+    
+class Enemigo:
+    def __init__(self, loot):
+               self._ vida= vida/2
+    
