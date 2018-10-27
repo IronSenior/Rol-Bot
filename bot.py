e6a7a2c36f8d0bd3281be5965895c0eb31b30ff4
Jose Marquez <pepemarquezof@gmail.com>
2018-10-27 19:40:09 +0200

    error de en desarrollo

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from libs import usuario, historia
 import telebot
 import json
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot("783258412:AAGqkILjGv4D6ru0jakNQqsGSPVBKfT_wlk")
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -16,14 +16,17 @@ def start(m):
 
 @bot.callback_query_handler(func = lambda op: op.data in ["op_1", "op_2", "op_3"])
 def parrafo_siguiente(op):
+    
     cid = op.message.chat.id
     ant_parrafo = usuario.getparrafo(cid)
+    try:
+        sig_parrafo = historia.getsigparrafo(ant_parrafo, op.data)
+        usuario.setparrafo(cid, sig_parrafo)
 
-    sig_parrafo = historia.getsigparrafo(ant_parrafo, op.data)
-    usuario.setparrafo(cid, sig_parrafo)
-
-    message, keyb = historia.getKayboard(parrafo= sig_parrafo)
-    bot.send_message(cid, message, reply_markup = keyb)
+        message, keyb = historia.getKayboard(parrafo= sig_parrafo)
+        bot.send_message(cid, message, reply_markup = keyb)
+    except:
+        bot.send_message(cid, "Has muerto por tonto (En desarrollo)")
 
 
 bot.polling(none_stop=True, timeout=100)
